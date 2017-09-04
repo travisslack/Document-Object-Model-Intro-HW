9610cc7170259a1bb7341a165425fd4a181d29d8
Travis Grimmond <travisslack1994@outlook.com>
2017-09-04 18:01:28 +0100

    Added style in the way that the writing is now red

--- public/app.js
+++ public/app.js
@@ -1,6 +1,6 @@
 var CATS_TO_ADD = [
-{name: "Seth", favefood: "Tuna", img: "http://66.media.tumblr.com/d1f01bbe0150fda0c40d2151c5eaeac8/tumblr_odlqqskjj61v9cejwo1_400.jpg"},
-{name: "Travis", favefood: "Quiche", img: "https://68.media.tumblr.com/88d0fcf2b84a7b098dda839130597569/tumblr_okuo4teiql1uhevdso1_1280.jpg"}
+{name: "name: Seth", favefood: "fave food: Tuna", img: "http://66.media.tumblr.com/d1f01bbe0150fda0c40d2151c5eaeac8/tumblr_odlqqskjj61v9cejwo1_400.jpg"},
+{name: "name: Travis", favefood: "fave food: Quiche", img: "https://68.media.tumblr.com/88d0fcf2b84a7b098dda839130597569/tumblr_okuo4teiql1uhevdso1_1280.jpg"}
 ];
 
 var app = function() {
@@ -10,8 +10,8 @@ var app = function() {
   var ul = document.createElement('ul');
     ul.classList.add('cat');
 
-  var li1 = document.createElement('li');
-    li1.innerText = name;
+  var nameLi = document.createElement('li');
+    nameLi.innerText = name;
 
   var li2 = document.createElement('li');
     li2.innerText = favefood;
@@ -22,7 +22,7 @@ var app = function() {
 
 
 
-  ul.appendChild(li1)
+  ul.appendChild(nameLi)
 
   ul.appendChild(li2);
 
